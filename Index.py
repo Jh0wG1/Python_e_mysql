407f4d71c6d25fd6687ebf2816671635f321de98
Jhow <jhones.gasparinidev@gmail.com>
2021-11-13 17:29:56 -0300

    tela inicial

---
+++ Index.py
@@ -0,0 +1,12 @@
+#Importando Bibliotecas
+from tkinter import *
+from tkinter import messagebox
+
+#Criar Nossa Janela
+jan = Tk()
+jan.title("Code Systen")
+jan.geometry("600x300")
+jan.configure(background="white")
+jan.resizable(width=False, height=False)
+
+jan.mainloop()
